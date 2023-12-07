21dde63bdf32097ff40a1f2c57b7b9320d6ea76b
Alex Lévy <alexlevy0@gmail.com>
2023-12-08 00:53:25 +0100

    Added frameloop="demand" on Canvas

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber'
-import { Preload } from '@react-three/drei'
 import { r3f } from '@/helpers/global'
+import { Preload } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
+    <Canvas
+      {...props}
+      frameloop="demand"
+    >
       {/* @ts-ignore */}
       <r3f.Out />
       <Preload all />
